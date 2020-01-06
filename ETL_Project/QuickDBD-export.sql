0015d24f985f6627a5cfa299d17d647ebd8363b9
Najmeh dehghani <55862735+nmansoorabadi@users.noreply.github.com>
2020-01-05 21:28:55 -0600

    Add files via upload

--- ETL_Project/QuickDBD-export.sql
+++ ETL_Project/QuickDBD-export.sql
@@ -2,33 +2,37 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Unemployment_rate" (
-    "county_id" INT   NOT NULL,
-    "month" VARCHAR   NOT NULL,
-    "state" VARCHAR   NOT NULL,
-    "county" VARCHAR   NOT NULL,
-    "monthly_rate" INT   NOT NULL
+CREATE TABLE Unemployment_rate (
+    county_id INT   NOT NULL,
+    month VARCHAR   NOT NULL,
+    state VARCHAR   NOT NULL,
+    county VARCHAR   NOT NULL,
+    monthly_rate INT   NOT NULL
 );
 
-CREATE TABLE "county_pop" (
-    "id" INT   PRIMARY KEY NOT NULL,
-    "state" VARCHAR   NOT NULL,
-    "county" VARCHAR   NOT NULL,
-    "totalpop" INT   NOT NULL,
-    "men" INT   NOT NULL,
-    "women" INT   NOT NULL,
+delete from Unemployment_rate;
+
+CREATE TABLE county_pop (
+    id INT   PRIMARY KEY NOT NULL,
+    state VARCHAR   NOT NULL,
+    county VARCHAR   NOT NULL,
+    totalpop INT   NOT NULL,
+    men INT   NOT NULL,
+    women INT   NOT NULL
   
 );
+delete from county_pop; 
 
-CREATE TABLE "annual_rate" (
-    "county_id" INT PRIMARY KEY NOT NULL,
-    "county" VARCHAR   NOT NULL,
-    "Annual_rate" INT   NOT NULL
+
+CREATE TABLE annual_rate (
+    county_id INT PRIMARY KEY NOT NULL,
+    county VARCHAR   NOT NULL,
+    Annual_rate INT   NOT NULL
 );
 
+delete from annual_rate;
 
-
-ALTER TABLE "Unemployment_rate" ADD CONSTRAINT "fk_Unemployment_rate_county_id" FOREIGN KEY("county_id")
+ALTER TABLE Unemployment_rate ADD CONSTRAINT "fk_Unemployment_rate_county_id" FOREIGN KEY("county_id")
 REFERENCES "county_pop" ("id");
 
 ALTER TABLE "annual_rate" ADD CONSTRAINT "fk_annual_rate_county_id" FOREIGN KEY("county_id")
